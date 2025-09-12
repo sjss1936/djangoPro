b7f8e859bed0d9cf604fc9f668258707ee5b4a99
sjss1936 <108500639+sjss1936@users.noreply.github.com>
2025-09-12 10:26:53 +0900

    up

--- Projects/mysite/pybo/views.py
+++ Projects/mysite/pybo/views.py
@@ -1,6 +1,97 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse
-from django.shortcuts import render
+# from django.http import HttpResponseNotAllowed
+from django.shortcuts import render, get_object_or_404, redirect
+from pyexpat.errors import messages
+from django.contrib import messages
+from pybo.models import Question
+from django.utils import timezone
+from .forms import QuestionForm, AnswerForm
+from django.contrib.auth.decorators import login_required
+
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("안녕하세요! pybo에 오신걸 환영합니다.")
+    """
+    pybo 목록 출력
+    """
+    page = request.GET.get('page', '1')
+    question_list = Question.objects.all().order_by('-create_date')
+
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj, 'page': page}
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    """
+       pybo 내용 출력
+       """
+    # question = Question.objects.get(id=question_id)  # question_id에 해당하는 객체 추출
+    question = get_object_or_404(Question, pk=question_id) # question_id에 해당하는 객체 추출
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+@login_required(login_url='common:login')
+def answer_create(request, question_id):
+    """
+    pybo 답변 등록
+    """
+    question = get_object_or_404(Question, pk=question_id)  # question_id에 해당하는 객체 추출
+    question.answer_set.create(content=request.POST.get('content'),
+                               create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
+
+@login_required(login_url='common:login')
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user  # author 속성에 로그인 계정 저장
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
+
+def answer_create (request, question_id):
+    """
+    pybo에 답변 등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user  # author 속성에 로그인 계정 저장
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+        return HttpResponseNotAllowed('Only POST is possible.')
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('pybo:detail', question_id=question.id)
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.modify_date = timezone.now()  # 수정일시 저장
+            question.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = QuestionForm(instance=question)
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
